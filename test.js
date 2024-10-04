5d1f6b7125d1d396c9d8ec954a5277d0f4358807
Denys Vysockij <131520948+denyxqq@users.noreply.github.com>
2024-10-04 16:40:46 +0300

    Add files via upload

---
+++ test.js
@@ -0,0 +1,41 @@
+const myAge = 16;
+
+console.log(myAge);
+
+const userName = "Денис";
+
+console.log(userName);
+
+const isStudent = true;
+
+console.log(isStudent);
+
+const myString = "сьогодні завтрашній день";
+
+console.log(myString);
+
+const myNumber = 5;
+
+console.log(myNumber);
+
+const b = 12;
+
+
+const sum = myNumber + b;
+
+console.log(sum);
+
+const myNull = null;
+
+console.log(myNull);
+
+let userNames = prompt("Будь ласка введіть ваше імя")
+console.log(userNames)
+
+let userConfirmed = confirm("Ви підтверджуєте дію?")
+alert(userConfirmed ? "Дякую за підтвердження!" : "Дію відмінено!")     
+
+let accountDelete = confirm("Ви дійсно хочете видалити свій акаунт?")
+alert("Увага! Ця дія небезпечна.")
+
+let accountDeleting = confirm("Ви підтверджуєте цю дію?")
